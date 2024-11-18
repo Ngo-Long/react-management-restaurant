518f2a5fa163d2195fd26b6117d70b983779168d
Ngo-Long <ngolong.hh@gmail.com>
2024-11-19 03:43:23 +0700

    config project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React + TypeScript + Vite
+
+Node: 20.14.0
--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
     },
   },
 )
